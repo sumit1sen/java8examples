833dd17151ad97df281fdd8660cb0bf04095f0e4
Sumit Sen <sumit1sen@users.noreply.github.com>
2015-08-26 22:54:17 -0700

    Initial project creation

---
+++ default/src/java8ex1.java
@@ -0,0 +1,18 @@
+/**
+ * This class is my first example code for learning java 8 features, in particular, streams
+ * Created by ssen on 8/26/2015.
+ */
+public class java8ex1 {
+    // create a collection
+    // create a stream from the collection
+    // perform map and reduce on stream
+    // compare to non-functional approach
+    // add more bells and whistles
+    // test with grid row/column processing
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
